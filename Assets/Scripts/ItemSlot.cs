e1c4e60c192012f5c71ae7f47c758a47636b95d0
three-co1ors <131871273+three-co1ors@users.noreply.github.com>
2025-11-08 21:40:31 +0900

    [FEAT] 연구실-인벤토리 연결
    - 정해야 할 것 -
    1. 진화 성공 시 진화 작물을 팝업에 띄우는 건지
    2. 작업대에 올려져 있는 작물/포션 터치하면 다시 회수되게끔 하는 건지

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -54,8 +54,9 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler
     public void ClearSlot()
     {
         item = null;
+
         itemIcon.sprite = null;
-        itemIcon.color = new Color(1, 1, 1, 0);
+        itemIcon.color = Color.white;
 
         if (quantityText != null)
         {
@@ -63,7 +64,6 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler
             quantityText.gameObject.SetActive(false);
         }
 
-        // 비울 때 선택 테두리도 해제
         if (selectionBorder != null)
         {
             selectionBorder.gameObject.SetActive(false);
@@ -82,35 +82,60 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler
     // 슬롯이 클릭되었을 때 호출되는 함수
     public void OnPointerClick(PointerEventData eventData)
     {
-        // 1. '메인 인벤토리' 슬롯을 클릭했을 때
+        // 1. '메인 인벤토리' 슬롯을 클릭했을 때 (기존과 동일)
         if (slotType == SlotType.Inventory)
         {
-            InventoryUI.Instance.SelectSlot(this);
+            // 한 번 클릭: 선택
+            if (eventData.clickCount == 1)
+            {
+                InventoryUI.Instance.SelectSlot(this);
+            }
         }
-        // 2. [추가] '연구실 안의 인벤토리' 슬롯을 클릭했을 때
+        // 2. '연구실 안의 인벤토리' 슬롯을 클릭했을 때
         else if (slotType == SlotType.Lab_Inventory)
         {
-            // ResearchLab의 선택 함수를 호출
-            ResearchLab.Instance.SelectSlot(this);
+            // 2a. 한 번 클릭 (선택)
+            if (eventData.clickCount == 1)
+            {
+                ResearchLab.Instance.SelectSlot(this);
+            }
+            // 2b. 두 번 클릭 (자동 배치)
+            else if (eventData.clickCount == 2 && this.item != null)
+            {
+                ResearchLab lab = ResearchLab.Instance;
+                ItemData itemToMove = this.item;
+
+                // 2c. 아이템 카테고리에 따라 적절한 혼합기 슬롯에 배치
+                if (itemToMove.itemCategory == "Crop" && lab.materialSlot.item == null)
+                {
+                    // 작물이면 '재료' 슬롯에 배치
+                    lab.materialSlot.SetItem(itemToMove, 1);
+                    InventoryManager.Instance.RemoveItem(itemToMove, 1);
+                    lab.ClearSelection();
+                }
+                else if (itemToMove.itemCategory == "Potion" && lab.potionSlot.item == null)
+                {
+                    // 포션이면 '포션' 슬롯에 배치
+                    lab.potionSlot.SetItem(itemToMove, 1);
+                    InventoryManager.Instance.RemoveItem(itemToMove, 1);
+                    lab.ClearSelection();
+                }
+            }
         }
         // 3. '연구실 재료' 슬롯 (왼쪽 혼합기)을 클릭했을 때
         else if (slotType == SlotType.Material)
         {
-            // 3a. 연구실 인벤토리에서 선택한 아이템이 있다면 (배치하기)
-            if (ResearchLab.Instance.selectedItem != null)
-            {
-                this.SetItem(ResearchLab.Instance.selectedItem, 1);
-                InventoryManager.Instance.RemoveItem(ResearchLab.Instance.selectedItem, 1);
-                ResearchLab.Instance.ClearSelection();
-            }
-            // 3b. 선택한 아이템이 없고, 이 슬롯에 아이템이 있다면 (회수하기)
-            else if (this.item != null)
+            // [!!! 수정 !!!]
+            if (this.item != null)
             {
                 InventoryManager.Instance.AddItem(this.item, 1);
                 this.ClearSlot();
+
+                if (ResearchLab.Instance != null)
+                    ResearchLab.Instance.ClearSelection();
             }
         }
-        // 4. '결과' 슬롯 (인벤토리/연구실 공통)
+        // 4. '결과' 슬롯 (기존과 동일)
         else if (slotType == SlotType.Result)
         {
             if (this.item != null)
