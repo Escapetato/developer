e3c2d2cc3ce97e55a29bc0e975a7318fa89d92fd
chaeyeon-jung <coduschung@naver.com>
2025-11-13 11:10:45 +0900

    QuestCommon 작성

--- Assets/Scripts/QuestSystem/QuestCommon.cs
+++ Assets/Scripts/QuestSystem/QuestCommon.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class QuestCommon : MonoBehaviour
+[Serializable] 
+public enum QuestType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Main, 
+    Sub, 
+    Daily   
 }
+
+[Serializable] 
+public enum QuestState
+{
+    Locked, 
+    Active,     
+    Completed, 
+    Closed    
+}
+
+[Serializable] 
+public class QuestData
+{
+    [Header("기본 정보")]
+    public int key;              // 고유 ID
+    public string title;        // 퀘스트 이름
+    public QuestType type;      // 메인 / 서브 / 일일
+    [TextArea]
+    public string questDesc;  // 퀘스트 설명
+
+    [Header("달성 조건")]
+    public int conditionCount;       // 목표 수치 
+
+    [Header("보상 정보")]
+    public int rewardKey;    // 보상 아이템 ID
+    public int rewardAmount;    // 보상 수량
+
+    [Header("체인 연결")]
+    public int nextKey;     // 다음 퀘스트 ID (없으면 0이나 -1로 처리)
+}
+
